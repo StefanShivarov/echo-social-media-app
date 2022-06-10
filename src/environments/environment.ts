8bf0bdc940a63edd60c3974ca2a6df0acf1dbdd6
StefanShivarov <stefan.shivarov.jr@gmail.com>
2022-06-10 18:27:15 +0300

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'echo-angular',
+    appId: '1:240633916792:web:8c68b10fae33640f336c8d',
+    storageBucket: 'echo-angular.appspot.com',
+    apiKey: 'AIzaSyB2dTCGFlJ-l1qErCz3_e-miUfKamR8FYY',
+    authDomain: 'echo-angular.firebaseapp.com',
+    messagingSenderId: '240633916792',
+    measurementId: 'G-HGT1P2P3L3',
+  },
   production: false
 };
 
