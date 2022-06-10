7bf9a4a88d432b7121c430f07017a8bc2279eec8
StefanShivarov <stefan.shivarov.jr@gmail.com>
2022-06-10 20:01:29 +0300

    Added routing

--- src/app/feature/pages/pages.module.ts
+++ src/app/feature/pages/pages.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    LandingPageComponent
   ],
   imports: [
     CommonModule
